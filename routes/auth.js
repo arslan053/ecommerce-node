cb9dd755ffa42a9800a7043e22bfe51d3afbaeb2
Muhammad Arslan <arslan05342@gmail.com>
2025-12-28 23:35:27 +0500

    Feature/Session and cookies are done with appropriate packages. presetup for authentication is colmpleted

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+const exress = require('express')
+
+const router = exress.Router()
+
+const authController = require('../controllers/auth')
+
+router.get('/login', authController.getLogin)
+router.post('/login', authController.postLogin)
+router.post('/logout', authController.postLogout)
+
+
+
+module.exports = router;
